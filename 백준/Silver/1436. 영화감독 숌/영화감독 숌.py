28af23638e626abf7739e7ca0973f16312dc08a8
yutaaa <dbfk1207@gmail.com>
2023-02-01 20:43:32 +0900

    [Silver V] Title: 영화감독 숌, Time: 796 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,15 +1,9 @@
 N = int(input())
 num = 0
 
-def check_num(num: str) -> bool:
-    if "666" in num:
-        return True
-    else:
-        return False
-
 while N > 0:
     num += 1
-    if check_num(str(num)):
+    if "666" in str(num):
         N -= 1
 
 print(num)
