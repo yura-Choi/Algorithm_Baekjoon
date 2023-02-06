f385158e6b2285571a0f13e652bda76da3a260ec
yutaaa <dbfk1207@gmail.com>
2023-02-07 02:46:12 +0900

    [Silver IV] Title: 제로, Time: 104 ms, Memory: 34096 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,12 @@
+import sys
+from collections import deque
+
+stack = deque()
+K = int(sys.stdin.readline())
+for i in range(K):
+    num = int(sys.stdin.readline())
+    if num == 0:
+        stack.pop()
+    else:
+        stack.append(num)
+print(sum(stack))
