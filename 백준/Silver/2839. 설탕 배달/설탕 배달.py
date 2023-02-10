81278c02325db621960caaca379cd674167acfaf
yutaaa <dbfk1207@gmail.com>
2023-02-10 23:02:45 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,25 +1,13 @@
 N = int(input())
 
-count, temp = 0, N
-flag = False
+count = 0
 
-while temp != 0:
-    if not flag and temp >= 5: # 5의 배수
-        count += 1
-        temp -= 5
-    elif temp % 3 == 0: # 5보다 작으면서 나머지가 3의 배수일 경우 or 5로는 나누어떨어지지 않으면서 나머지가 3의 배수인 경우
-        if temp == N:
-            count = temp // 3
-        else:
-            count += temp // 3
-        temp = 0 # 종료 조건
-    else: # 5보다 작지만 나머지가 3의 배수로 나오지 않는 경우
-        temp += 5
-        count -= 1
-        flag = True
-
-    if flag and temp > N: # 3, 5로 표현되지 않는 수일 경우
-        count = -1
+while N > 0:
+    if N % 5 == 0:
+        count += N // 5
+        N = 0
         break
+    N -= 3
+    count += 1
 
-print(count)
+print(count if N == 0 else -1)
