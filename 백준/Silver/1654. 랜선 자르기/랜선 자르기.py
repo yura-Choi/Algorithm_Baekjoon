a51e6023b14f9d5202d2cc29f7bc053e29b6d468
yutaaa <dbfk1207@gmail.com>
2023-02-11 04:16:35 +0900

    [Silver II] Title: 랜선 자르기, Time: 92 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -4,10 +4,9 @@ count_tohave, count_tomake = map(int, sys.stdin.readline().split())
 cable = []
 for _ in range(count_tohave):
     cable.append(int(sys.stdin.readline()))
-cable.sort()
 
 # parametric search
-start, end = 1, cable[count_tohave-1]
+start, end = 1, max(cable)
 save_status = -1
 while start <= end:
     mid = (start + end) // 2
