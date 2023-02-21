b1b710a04156951287624147e5da922ac560061d
yutaaa <dbfk1207@gmail.com>
2023-02-21 13:40:15 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 40 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -1,12 +1,11 @@
 N = int(input())
 cache = [0] * (N+1)
+cache[1] = 1
 
-for i in range(1, N+1):
-    if i == 1:
-        cache[i] = 1
-    elif i == 2:
+for i in range(2, N+1):
+    if i == 2:
         cache[i] = 3
     else:
-        cache[i] = cache[i-2] * 2 + cache[i-1]
+        cache[i] = (cache[i-2] * 2 + cache[i-1]) % 10007
 
-print(cache[N] % 10007)
+print(cache[N])
